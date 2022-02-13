37816180b512d8bf500b60b2071f8a38b92c3ba3
Miguel Lopes <miguelpvz@hotmail.com>
2022-02-13 19:45:03 +0100

    Configure DBContext

--- PlatformService/Data/AppDbContext.cs
+++ PlatformService/Data/AppDbContext.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+
 namespace PlatformService.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
-        
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+            
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
     }
 }
