9a76fc37275f96baf8fd776d52a654919183281e
Miguel Lopes <miguelpvz@hotmail.com>
2022-02-23 20:12:30 +0100

    data layer dtos

---
+++ CommandsService/Dtos/CommandCreateDto.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CommandsService.Dtos
+{
+    public class CommandCreateDto
+    {
+        [Required]public string HowTo { get; set; }
+        [Required]public string CommandLine { get; set; }
+    }
+}
