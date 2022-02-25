b5b9785b67a036c6618da77736ed498e104d85fd
Miguel Lopes <miguelpvz@hotmail.com>
2022-02-26 00:18:41 +0100

    further tune grpc client

--- CommandsService/Profiles/CommandsProfile.cs
+++ CommandsService/Profiles/CommandsProfile.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using CommandsService.Dtos;
 using CommandsService.Models;
+using PlatformService;
 
 namespace CommandsService.Profiles
 {
@@ -16,6 +17,13 @@ namespace CommandsService.Profiles
                 .ForMember(dest => dest.ExternalID, 
                     opt => opt.MapFrom(
                         src => src.Id));
+            CreateMap<GrpcPlatformModel, Platform>()
+                .ForMember(dest => dest.ExternalID,
+                    opt => opt.MapFrom(src => src.PlatformId))
+                .ForMember(dest => dest.Name,
+                    opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Commands,
+                    opt => opt.Ignore());;;
         }
     }
 }
