18f96c6cf0f4807a44edccf0e4b46f1bb2fb4f00
Miguel Lopes <miguelpvz@hotmail.com>
2022-02-26 00:28:33 +0100

    prepare tests

--- CommandsService/Startup.cs
+++ CommandsService/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using CommandsService.AsyncDataServices;
 using CommandsService.Data;
 using CommandsService.EventProcessing;
+using CommandsService.SyncDataServices.Grpc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -38,6 +39,8 @@ namespace CommandsService
 
             services.AddSingleton<IEventProcessor, EventProcessor>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddScoped<IPlatformDataClient, PlatformDataClient>();
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CommandsService", Version = "v1" });
@@ -64,6 +67,8 @@ namespace CommandsService
             {
                 endpoints.MapControllers();
             });
+            
+            PrepDb.PrepPopulation(app);
         }
     }
 }
